e436f997329053f825d687c8b49fa6e3c8f874b9
crsnplusplus@gmail.com <crsnplusplus@gmail.com>
2018-02-20 16:27:37 +0100

    removed old files

--- src/last_utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "last_utils.h"
-#include <iostream>
-#include <assert.h>
-
-void LastPrintMessage(const char* Message)
-{
-  assert(Message != nullptr);
-  std::cout << "L>" << Message << std::endl;
-}
--- src/last_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LAST_UTILS_H__
-#define __LAST_UTILS_H__
-
-void LastPrintMessage(const char* Message);
-
-#endif // __LAST_UTILS_H__
